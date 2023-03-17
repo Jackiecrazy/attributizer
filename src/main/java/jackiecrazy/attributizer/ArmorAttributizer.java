cd4f7ada70b38959e8b5f6f32f31477b29890ee4
jackiecrazy <jackiecrazyhi@gmail.com>
2023-03-16 23:37:40 -0700

    yay

---
+++ src/main/java/jackiecrazy/attributizer/ArmorAttributizer.java
@@ -0,0 +1,98 @@
+package jackiecrazy.attributizer;
+
+import com.google.gson.*;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.event.AddReloadListenerEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ArmorAttributizer extends SimpleJsonResourceReloadListener {
+    public static final UUID[] MODIFIERS = {
+            UUID.fromString("a516026a-bee2-4014-bcb6-b6a5775553da"),
+            UUID.fromString("a516026a-bee2-4014-bcb6-b6a5775553db"),
+            UUID.fromString("a516026a-bee2-4014-bcb6-b6a5775553dc"),
+            UUID.fromString("a516026a-bee2-4014-bcb6-b6a5775553dd"),
+            UUID.fromString("a516026a-bee2-4014-bcb6-b6a5775553de")
+    };
+    public static final Map<Item, Map<Attribute, List<AttributeModifier>>> MAP = new HashMap<>();
+    public static Gson GSON = new GsonBuilder().registerTypeAdapter(ResourceLocation.class, new ResourceLocation.Serializer()).create();
+
+    public ArmorAttributizer() {
+        super(GSON, "armor");
+    }
+
+    public static void register(AddReloadListenerEvent event) {
+        event.addListener(new ArmorAttributizer());
+    }
+
+    @Override
+    protected void apply(Map<ResourceLocation, JsonElement> object, ResourceManager rm, ProfilerFiller profiler) {
+        MAP.clear();
+        object.forEach((key, value) -> {
+            JsonObject file = value.getAsJsonObject();
+            file.entrySet().forEach(entry -> {
+                final String name = entry.getKey();
+                ResourceLocation i = new ResourceLocation(name);
+                Item item = ForgeRegistries.ITEMS.getValue(i);
+                if (item == null) {
+                    Attributizer.LOGGER.debug(name + " is not a registered item!");
+                    return;
+                }
+                JsonArray array = entry.getValue().getAsJsonArray();
+                for (JsonElement e : array) {
+                    try {
+                        JsonObject obj = e.getAsJsonObject();
+                        final ResourceLocation attribute = new ResourceLocation(obj.get("attribute").getAsString());
+                        Attribute a = ForgeRegistries.ATTRIBUTES.getValue(attribute);
+                        if (a == null) {
+                            Attributizer.LOGGER.debug(attribute + " is not a registered attribute!");
+                            continue;
+                        }
+
+                        UUID uid;
+                        try {
+                            final String u = obj.get("uuid").getAsString();
+                            uid = UUID.fromString(u);
+                        } catch (Exception ignored) {
+                            //have to grab the uuid haiyaaa
+                            if (item instanceof ArmorItem armor) {
+                                uid = MODIFIERS[armor.getEquipmentSlot().getIndex()];
+                            } else uid = MODIFIERS[4];
+                        }
+                        double modify = obj.get("modify").getAsDouble();
+                        String type = obj.get("operation").getAsString();
+                        AttributeModifier am = new AttributeModifier(uid, "attributizer change", modify, AttributeModifier.Operation.valueOf(type));
+                        MAP.putIfAbsent(item, new HashMap<>());
+                        Map<Attribute, List<AttributeModifier>> sub = MAP.get(item);
+                        sub.putIfAbsent(a, new ArrayList<>());
+                        sub.get(a).add(am);
+                        MAP.put(item, sub);
+                    } catch (Exception x) {
+                        Attributizer.LOGGER.error("incomplete or malformed json under " + name + "!");
+                        x.printStackTrace();
+                    }
+                }
+            });
+        });
+    }
+
+    public static class AttributeMod {
+        public UUID uid;
+        public double mod;
+        public ResourceLocation attr;
+
+
+    }
+}
---
+++ src/main/java/jackiecrazy/attributizer/Attributizer.java
@@ -0,0 +1,51 @@
+package jackiecrazy.attributizer;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.AddReloadListenerEvent;
+import net.minecraftforge.event.ItemAttributeModifierEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.List;
+import java.util.Map;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Attributizer.MODID)
+public class Attributizer {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "attributizer";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    // Create a Deferred Register to hold Blocks which will all be registered under the "attributizer" namespace
+    public Attributizer() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Mod.EventBusSubscriber
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onJsonListener(AddReloadListenerEvent event) {
+            ArmorAttributizer.register(event);
+        }
+
+        @SubscribeEvent
+        public static void items(ItemAttributeModifierEvent e) {
+            if (ArmorAttributizer.MAP.containsKey(e.getItemStack().getItem()) && (!e.getOriginalModifiers().isEmpty() || (!(e.getItemStack().getItem() instanceof ArmorItem) && e.getSlotType() == EquipmentSlot.OFFHAND))) {//presumably this is the correct equipment slot
+                Map<Attribute, List<AttributeModifier>> map = ArmorAttributizer.MAP.get(e.getItemStack().getItem());
+                map.forEach((k, v) -> v.forEach(am -> e.addModifier(k, am)));
+            }
+        }
+    }
+}
