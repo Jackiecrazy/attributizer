bc3cec98cdc4cdf9f270ca40f91985415f2707b4
jackiecrazy <jackiecrazyhi@gmail.com>
2024-07-09 20:34:02 +0700

    12.1 complete

--- src/main/java/jackiecrazy/attributizer/EntityAttributizer.java
+++ src/main/java/jackiecrazy/attributizer/EntityAttributizer.java
@@ -1,31 +1,30 @@
 package jackiecrazy.attributizer;
 
 import com.google.gson.*;
-import net.minecraft.core.Registry;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
-import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
-import net.minecraftforge.event.AddReloadListenerEvent;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.neoforged.neoforge.event.AddReloadListenerEvent;
+import org.checkerframework.checker.units.qual.A;
+import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.*;
 
 public class EntityAttributizer extends SimpleJsonResourceReloadListener {
+    private static final ResourceLocation DEFAULT=ResourceLocation.fromNamespaceAndPath(Attributizer.MODID, "mob_change");
     public static final UUID MODIFIER = UUID.fromString("a516026a-bee2-4014-bcb6-b6a5776663da");
-    public static final Map<Attribute, List<AttributeMod>> GLOBALMAP = new HashMap<>();
-    public static final Map<EntityType<?>, Map<Attribute, List<AttributeMod>>> MAP = new HashMap<>();
-    public static final Map<TagKey<EntityType<?>>, Map<Attribute, List<AttributeMod>>> ARCHETYPES = new HashMap<>();
+    public static final Map<Holder<Attribute>, List<AttributeMod>> GLOBALMAP = new HashMap<>();
+    public static final Map<EntityType<?>, Map<Holder<Attribute>, List<AttributeMod>>> MAP = new HashMap<>();
+    public static final Map<TagKey<EntityType<?>>, Map<Holder<Attribute>, List<AttributeMod>>> ARCHETYPES = new HashMap<>();
     public static Gson GSON = new GsonBuilder().registerTypeAdapter(ResourceLocation.class, new ResourceLocation.Serializer()).create();
 
     public EntityAttributizer() {
@@ -54,19 +53,20 @@ public class EntityAttributizer extends SimpleJsonResourceReloadListener {
                 for (JsonElement e : array) {
                     try {
                         JsonObject obj = e.getAsJsonObject();
-                        final ResourceLocation attribute = new ResourceLocation(obj.get("attribute").getAsString());
-                        Attribute a = ForgeRegistries.ATTRIBUTES.getValue(attribute);
-                        if (a == null) {
+                        final ResourceLocation attribute = ResourceLocation.parse(obj.get("attribute").getAsString());
+                        Optional<Holder.Reference<Attribute>> opt = BuiltInRegistries.ATTRIBUTE.getHolder(attribute);
+                        if (opt.isEmpty()) {
+                            Attributizer.LOGGER.debug("{} is not a registered attribute!", attribute);
                             continue;
                         }
+                        Holder<Attribute> a=opt.get();
 
-                        UUID uid;
+                        ResourceLocation uid;
                         try {
-                            final String u = obj.get("uuid").getAsString();
-                            uid = UUID.fromString(u);
+                            uid= ResourceLocation.parse(obj.get("resource_location").getAsString());
                         } catch (Exception ignored) {
-                            //default uuid
-                            uid = MODIFIER;
+                            //have to grab the uuid haiyaaa
+                            uid = Attributizer.MODIFIERS[4];
                         }
                         double min=0, max=0;
                         if (obj.has("min")) {
@@ -82,19 +82,19 @@ public class EntityAttributizer extends SimpleJsonResourceReloadListener {
                             GLOBALMAP.get(a).add(am);
                         }
                         else if(isTag){
-                            TagKey<EntityType<?>> tag= TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation(name));
+                            TagKey<EntityType<?>> tag= TagKey.create(Registries.ENTITY_TYPE, ResourceLocation.parse(name));
                             ARCHETYPES.putIfAbsent(tag, new HashMap<>());
-                            Map<Attribute, List<AttributeMod>> sub = ARCHETYPES.get(tag);
+                            Map<Holder<Attribute>, List<AttributeMod>> sub = ARCHETYPES.get(tag);
                             sub.putIfAbsent(a, new ArrayList<>());
                             sub.get(a).add(am);
                             ARCHETYPES.put(tag, sub);
                         }
                         else {
-                            ResourceLocation i = new ResourceLocation(name);
-                            if (ForgeRegistries.ENTITY_TYPES.containsKey(i)) {
-                                EntityType<?> mob = ForgeRegistries.ENTITY_TYPES.getValue(i);
+                            ResourceLocation i = ResourceLocation.parse(name);
+                            if (BuiltInRegistries.ENTITY_TYPE.containsKey(i)) {
+                                EntityType<?> mob = BuiltInRegistries.ENTITY_TYPE.get(i);
                                 MAP.putIfAbsent(mob, new HashMap<>());
-                                Map<Attribute, List<AttributeMod>> sub = MAP.get(mob);
+                                Map<Holder<Attribute>, List<AttributeMod>> sub = MAP.get(mob);
                                 sub.putIfAbsent(a, new ArrayList<>());
                                 sub.get(a).add(am);
                                 MAP.put(mob, sub);
@@ -111,6 +111,9 @@ public class EntityAttributizer extends SimpleJsonResourceReloadListener {
     }
 
     public enum Operation{
+        ADD_VALUE,
+        ADD_MULTIPLIED_BASE,
+        ADD_MULTIPLIED_TOTAL,
         ADDITION,
         MULTIPLY_BASE,
         MULTIPLY_TOTAL,
@@ -118,12 +121,12 @@ public class EntityAttributizer extends SimpleJsonResourceReloadListener {
     }
 
     public static class AttributeMod {
-        public UUID uid;
+        public ResourceLocation uid;
         public double min;
         public double scale;
         public Operation operation;
 
-        public AttributeMod(UUID uid, double min, double max, Operation operation) {
+        public AttributeMod(ResourceLocation uid, double min, double max, Operation operation) {
             this.uid = uid;
             this.min = min;
             this.scale = max - min;
@@ -135,7 +138,7 @@ public class EntityAttributizer extends SimpleJsonResourceReloadListener {
             if (Objects.requireNonNull(operation) == Operation.SET_BASE) {
                 i.setBaseValue(random);
             } else if(i.getModifier(uid)==null){
-                i.addPermanentModifier(new AttributeModifier(uid, "attributizer change", random, AttributeModifier.Operation.fromValue(operation.ordinal())));
+                i.addPermanentModifier(new AttributeModifier(uid, random, OperationBridge.valueOf(operation.name()).translate()));
             }
 
         }
